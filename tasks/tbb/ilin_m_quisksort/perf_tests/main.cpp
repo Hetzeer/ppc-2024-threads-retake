20f1a946a276065bfecc8f2caf43a05703e09f92
multifacs <maksim.ilin.01@yandex.ru>
2024-10-26 15:23:30 +0300

    initial tbb

---
+++ tasks/tbb/ilin_m_quisksort/perf_tests/main.cpp
@@ -0,0 +1,86 @@
+// Copyright 2024 Ilin Maksim
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "core/perf/include/perf.hpp"
+#include "tbb/ilin_m_quisksort/include/quicksort.hpp"
+
+TEST(tbb_ilin_m_quicksort_perf, test_pipeline_run) {
+  // const int count = 1000;
+
+  // // Create data
+  // std::vector<int> in = ilin_m_quisksort_seq::getRandomVec(count);
+  // std::vector<int> out(count, 0);
+
+  // // Create TaskData
+  // std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  // taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  // taskDataSeq->inputs_count.emplace_back(in.size());
+  // taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  // taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // // Create Task
+  // auto testTaskSequential = std::make_shared<ilin_m_quisksort_seq::QuisksortSequential>(taskDataSeq);
+
+  // // Create Perf attributes
+  // auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  // perfAttr->num_running = 10;
+  // const auto t0 = std::chrono::high_resolution_clock::now();
+  // perfAttr->current_timer = [&] {
+  //   auto current_time_point = std::chrono::high_resolution_clock::now();
+  //   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+  //   return static_cast<double>(duration) * 1e-9;
+  // };
+
+  // // Create and init perf results
+  // auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // // Create Perf analyzer
+  // auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+  // perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  // ppc::core::Perf::print_perf_statistic(perfResults);
+  // ASSERT_TRUE(ilin_m_quisksort_seq::checkOrder(out));
+}
+
+TEST(tbb_ilin_m_quicksort_perf, test_task_run) {
+  // const int count = 1000;
+
+  // // Create data
+  // std::vector<int> in = ilin_m_quisksort_seq::getRandomVec(count);
+  // std::vector<int> out(count, 0);
+
+  // // Create TaskData
+  // std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  // taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  // taskDataSeq->inputs_count.emplace_back(in.size());
+  // taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  // taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // // Create Task
+  // auto testTaskSequential = std::make_shared<ilin_m_quisksort_seq::QuisksortSequential>(taskDataSeq);
+
+  // // Create Perf attributes
+  // auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  // perfAttr->num_running = 10;
+  // const auto t0 = std::chrono::high_resolution_clock::now();
+  // perfAttr->current_timer = [&] {
+  //   auto current_time_point = std::chrono::high_resolution_clock::now();
+  //   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+  //   return static_cast<double>(duration) * 1e-9;
+  // };
+
+  // // Create and init perf results
+  // auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // // Create Perf analyzer
+  // auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+  // perfAnalyzer->task_run(perfAttr, perfResults);
+  // ppc::core::Perf::print_perf_statistic(perfResults);
+  // ASSERT_TRUE(ilin_m_quisksort_seq::checkOrder(out));
+}
+
+// int main(int argc, char **argv) {
+//   testing::InitGoogleTest(&argc, argv);
+//   return RUN_ALL_TESTS();
+// }
---
+++ tasks/tbb/ilin_m_quisksort/src/quicksort.cpp
@@ -0,0 +1,178 @@
+// Copyright 2024 Ilin Maksim
+#include "tbb/ilin_m_quisksort/include/quicksort.hpp"
+
+#include <omp.h>
+#include <tbb/tbb.h>
+
+#include <functional>
+#include <iterator>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+std::vector<int> ilin_m_quicksort_tbb::getRandomVec(int n) {
+  std::random_device dev;
+  static std::mt19937 gen(dev());
+  std::vector<int> vec(n);
+  std::generate(vec.begin(), vec.end(), [] { return gen() % 100; });
+  return vec;
+}
+
+bool ilin_m_quicksort_tbb::checkOrder(const std::vector<int>& vec) {
+  for (auto iter = vec.begin(); iter != std::prev(vec.end()); ++iter) {
+    if (*iter > *(iter + 1)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void swapElements(std::vector<int>::iterator a, std::vector<int>::iterator b) { std::iter_swap(a, b); }
+
+std::vector<int>::iterator partition(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+  // Choose the last element as the pivot
+  std::vector<int>::iterator pivot = end - 1;
+  std::vector<int>::iterator i = begin - 1;  // Index of smaller element
+
+  for (std::vector<int>::iterator j = begin; j != end - 1; ++j) {
+    // If current element is smaller than or equal to pivot
+    if (*j <= *pivot) {
+      i++;                 // increment index of smaller element
+      swapElements(i, j);  // Swap elements at i and j
+    }
+  }
+  swapElements(i + 1, pivot);  // Swap the pivot element with the greater element at i+1
+  return i + 1;                // Return the iterator to the pivot's final position
+}
+
+void ilin_m_quicksort_tbb::quickSort(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+  if (begin >= end - 1) {  // Base case: 0 or 1 element(s) in the range
+    return;                // Already sorted
+  }
+
+  // Partition the vector and get the pivot's final position
+  std::vector<int>::iterator pivot = partition(begin, end);
+
+  // Recursively sort the sub-vectors
+  quickSort(begin, pivot);    // Sort elements before the pivot
+  quickSort(pivot + 1, end);  // Sort elements after the pivot
+}
+
+std::vector<int> ilin_m_quicksort_tbb::mergeVecs(std::vector<int> arr1, std::vector<int> arr2) {
+  int n1 = static_cast<int>(arr1.size());
+  int n2 = static_cast<int>(arr2.size());
+
+  int i = 0, j = 0, k = 0;
+
+  std::vector<int> arr3(n1 + n2);
+
+  while (i < n1 && j < n2) {
+    if (arr1[i] < arr2[j])
+      arr3[k++] = arr1[i++];
+    else
+      arr3[k++] = arr2[j++];
+  }
+
+  while (i < n1) arr3[k++] = arr1[i++];
+  while (j < n2) arr3[k++] = arr2[j++];
+
+  return arr3;
+}
+
+void ilin_m_quicksort_tbb::quickSortSimpleMerge(std::vector<int>* vec, int parts, bool parallel) {
+  std::vector<std::vector<int>> vecs(parts);
+  int grainsize = static_cast<int>((*vec).size()) / parts;
+
+  if (!grainsize) {
+    quickSort(vec->begin(), vec->end());
+    return;
+  }
+
+  for (int i = 0; i < parts - 1; i++) {
+    std::vector<int>::const_iterator first = (*vec).begin() + grainsize * i;
+    std::vector<int>::const_iterator last = (*vec).begin() + grainsize * (i + 1);
+
+    vecs[i] = std::vector<int>(first, last);
+  }
+  vecs.back() = std::vector<int>((*vec).begin() + grainsize * (parts - 1), (*vec).end());
+
+  if (parallel) {
+    tbb::task_group group;
+    for (int i = 0; i < parts; i++) {
+      group.run([&vecs, i]() { quickSort(vecs[i].begin(), vecs[i].end()); });
+    }
+    group.wait();
+  } else {
+    for (auto&& elem : vecs) {
+      quickSort(elem.begin(), elem.end());
+    }
+  }
+
+  (*vec) = vecs[0];
+
+  for (auto iter = vecs.begin() + 1; iter != vecs.end(); ++iter) {
+    (*vec) = mergeVecs((*vec), *iter);
+  }
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskSequential::pre_processing() {
+  internal_order_test();
+  // Init value for input and output
+
+  input_.reserve(taskData->inputs_count[0]);
+  std::copy(reinterpret_cast<int*>(taskData->inputs[0]),
+            reinterpret_cast<int*>(taskData->inputs[0]) + taskData->inputs_count[0], std::back_inserter(input_));
+
+  return true;
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskSequential::validation() {
+  internal_order_test();
+  return taskData->inputs_count[0] == taskData->outputs_count[0];
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskSequential::run() {
+  internal_order_test();
+
+  ilin_m_quicksort_tbb::quickSort(input_.begin(), input_.end());
+  // for (auto el : res) {
+  //   std::cout << el << "\n";
+  // }
+  // std::this_thread::sleep_for(20ms);
+  return true;
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskSequential::post_processing() {
+  internal_order_test();
+  std::copy(input_.begin(), input_.end(), reinterpret_cast<int*>(taskData->outputs[0]));
+  return true;
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskParallel::pre_processing() {
+  internal_order_test();
+  // Init value for input and output
+
+  input_.reserve(taskData->inputs_count[0]);
+  std::copy(reinterpret_cast<int*>(taskData->inputs[0]),
+            reinterpret_cast<int*>(taskData->inputs[0]) + taskData->inputs_count[0], std::back_inserter(input_));
+
+  return true;
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskParallel::validation() {
+  internal_order_test();
+  return taskData->inputs_count[0] == taskData->outputs_count[0];
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskParallel::run() {
+  internal_order_test();
+
+  ilin_m_quicksort_tbb::quickSortSimpleMerge(&input_, omp_get_max_threads());
+  return true;
+}
+
+bool ilin_m_quicksort_tbb::QuisksortTBBTaskParallel::post_processing() {
+  internal_order_test();
+  std::copy(input_.begin(), input_.end(), reinterpret_cast<int*>(taskData->outputs[0]));
+  return true;
+}
