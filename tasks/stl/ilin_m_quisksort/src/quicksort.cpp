dbed684bcf62faa60a7019be8d77d0c2178be1b0
multifacs <maksim.ilin.01@yandex.ru>
2024-10-26 16:47:05 +0300

    fix

--- tasks/stl/ilin_m_quisksort/src/quicksort.cpp
+++ tasks/stl/ilin_m_quisksort/src/quicksort.cpp
@@ -97,7 +97,7 @@ void ilin_m_quicksort_stl::quickSortSimpleMerge(std::vector<int>* vec, int parts
 
   if (parallel) {
     std::vector<std::thread> threads;
-    threads.reserve(parts); // Pre-allocate capacity for 'parts' number of threads
+    threads.reserve(parts);  // Pre-allocate capacity for 'parts' number of threads
     for (int i = 0; i < parts; i++) {
       threads.emplace_back(std::thread([&vecs, i]() { quickSort(vecs[i].begin(), vecs[i].end()); }));
     }
